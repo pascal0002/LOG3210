00eeb5995d14139f64c5c7f0cf33535fbcf972f5
Pasc <pascal-alexandre.morel@polymtl.ca>
2020-09-15 14:33:31 -0400

    removed useless stuff

---
+++ TP2/TP2_Source/src/analyzer/visitors/SemantiqueVisitor.java
@@ -0,0 +1,285 @@
+package analyzer.visitors;
+
+import analyzer.SemantiqueError;
+import analyzer.ast.*;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created: 19-01-10
+ * Last Changed: 19-01-25
+ * Author: Félix Brunet
+ * <p>
+ * Description: Ce visiteur explorer l'AST est renvois des erreur lorqu'une erreur sémantique est détecté.
+ */
+
+public class SemantiqueVisitor implements ParserVisitor {
+
+    private final PrintWriter m_writer;
+
+    private HashMap<String, VarType> SymbolTable = new HashMap<>(); // mapping variable -> type
+
+    // variable pour les metrics
+    public int VAR = 0;
+    public int WHILE = 0;
+    public int IF = 0;
+    public int OP = 0;
+
+    public SemantiqueVisitor(PrintWriter writer) {
+        m_writer = writer;
+    }
+
+    /*
+    Le Visiteur doit lancer des erreurs lorsqu'un situation arrive.
+
+    pour vous aider, voici le code a utilisé pour lancer les erreurs
+
+    //utilisation d'identifiant non défini
+    throw new SemantiqueError("Invalid use of undefined Identifier " + node.getValue());
+
+    //utilisation de nombre dans la condition d'un if ou d'un while
+    throw new SemantiqueError("Invalid type in condition");
+
+    //assignation d'une valeur a une variable qui a déjà recu une valeur d'un autre type
+    ex : a = 1; a = true;
+    throw new SemantiqueError("Invalid type in assignment");
+
+    //expression invalide : (note, le code qui l'utilise est déjà fournis)
+    throw new SemantiqueError("Invalid type in expression got " + type.toString() + " was expecting " + expectedType);
+
+     */
+
+    @Override
+    public Object visit(SimpleNode node, Object data) {
+        return data;
+    }
+
+    @Override
+    public Object visit(ASTProgram node, Object data) {
+        node.childrenAccept(this, data);
+        m_writer.print(String.format("{VAR:%d, WHILE:%d, IF:%d, OP:%d}", this.VAR, this.WHILE, this.IF, this.OP));
+        return null;
+    }
+
+    /*
+    Doit enregistrer les variables avec leur type dans la table symbolique.
+     */
+    @Override
+    public Object visit(ASTDeclaration node, Object data) {
+        String varName = ((ASTIdentifier) node.jjtGetChild(0)).getValue();
+
+        SymbolTable.put(varName, node.getValue().equals("num") ? VarType.Number : VarType.Bool);
+
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTBlock node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+
+    @Override
+    public Object visit(ASTStmt node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+//    private void callChildenCond(SimpleNode node) {
+//        DataStruct d = new DataStruct();
+//        node.jjtGetChild(0).jjtAccept(this, d);
+//        int numChildren = node.jjtGetNumChildren();
+//        for (int i = 1; i < numChildren; i++) {
+//            node.jjtGetChild(i).jjtAccept(this, data);
+//        }
+//    }
+
+    /*
+    les structures conditionnelle doivent vérifier que leur expression de condition est de type booléenne
+    On doit aussi compter les conditions dans les variables IF et WHILE
+     */
+    @Override
+    public Object visit(ASTIfStmt node, Object data) {
+        DataStruct d = new DataStruct();
+        node.jjtGetChild(0).jjtAccept(this, d);
+        int numChildren = node.jjtGetNumChildren();
+        for (int i = 1; i < numChildren; i++) {
+            node.jjtGetChild(i).jjtAccept(this, data);
+        }
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTWhileStmt node, Object data) {
+        DataStruct d = new DataStruct();
+        node.jjtGetChild(0).jjtAccept(this, d);
+        int numChildren = node.jjtGetNumChildren();
+        for (int i = 1; i < numChildren; i++) {
+            node.jjtGetChild(i).jjtAccept(this, data);
+        }
+        return null;
+    }
+
+    /*
+    On doit vérifier que le type de la variable est compatible avec celui de l'expression.
+     */
+    @Override
+    public Object visit(ASTAssignStmt node, Object data) {
+        String varName = ((ASTIdentifier) node.jjtGetChild(0)).getValue();
+        DataStruct d = new DataStruct();
+        node.jjtGetChild(1).jjtAccept(this, d);
+
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTExpr node, Object data) {
+        //Il est normal que tous les noeuds jusqu'à expr retourne un type.
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTCompExpr node, Object data) {
+        /*attention, ce noeud est plus complexe que les autres.
+        si il n'a qu'un seul enfant, le noeud a pour type le type de son enfant.
+
+        si il a plus d'un enfant, alors ils s'agit d'une comparaison. il a donc pour type "Bool".
+
+        de plus, il n'est pas acceptable de faire des comparaisons de booleen avec les opérateur < > <= >=.
+        les opérateurs == et != peuvent être utilisé pour les nombres et les booléens, mais il faut que le type soit le même
+        des deux côté de l'égalité/l'inégalité.
+        */
+
+        int numChildren = node.jjtGetNumChildren();
+        for (int i = 0; i < numChildren; i++) {
+            DataStruct d = new DataStruct();
+            node.jjtGetChild(i).jjtAccept(this, d);
+        }
+
+        return null;
+    }
+
+
+    /*
+    opérateur binaire
+    si il n'y a qu'un enfant, aucune vérification à faire.
+    par exemple, un AddExpr peut retourné le type "Bool" à condition de n'avoir qu'un seul enfant.
+     */
+    @Override
+    public Object visit(ASTAddExpr node, Object data) {
+        int numChildren = node.jjtGetNumChildren();
+        for (int i = 0; i < numChildren; i++) {
+            DataStruct d = new DataStruct();
+            node.jjtGetChild(i).jjtAccept(this, d);
+        }
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTMulExpr node, Object data) {
+        int numChildren = node.jjtGetNumChildren();
+        for (int i = 0; i < numChildren; i++) {
+            DataStruct d = new DataStruct();
+            node.jjtGetChild(i).jjtAccept(this, d);
+        }
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTBoolExpr node, Object data) {
+        int numChildren = node.jjtGetNumChildren();
+        for (int i = 0; i < numChildren; i++) {
+            DataStruct d = new DataStruct();
+            node.jjtGetChild(i).jjtAccept(this, d);
+        }
+        return null;
+    }
+
+    /*
+    opérateur unaire
+    les opérateur unaire ont toujours un seul enfant.
+
+    Cependant, ASTNotExpr et ASTUnaExpr ont la fonction "getOps()" qui retourne un vecteur contenant l'image (représentation str)
+    de chaque token associé au noeud.
+
+    Il est utile de vérifier la longueur de ce vecteur pour savoir si une opérande est présente.
+
+    si il n'y a pas d'opérande, ne rien faire.
+    si il y a une (ou plus) opérande, ils faut vérifier le type.
+
+    */
+    @Override
+    public Object visit(ASTNotExpr node, Object data) {
+        node.jjtGetChild(0).jjtAccept(this, data);
+
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTUnaExpr node, Object data) {
+        node.jjtGetChild(0).jjtAccept(this, data);
+
+        return null;
+    }
+
+    /*
+    les noeud ASTIdentifier aillant comme parent "GenValue" doivent vérifier leur type.
+
+    Ont peut envoyé une information a un enfant avec le 2e paramètre de jjtAccept ou childrenAccept.
+     */
+    @Override
+    public Object visit(ASTGenValue node, Object data) {
+        node.childrenAccept(this, data);
+        return null;
+    }
+
+
+    @Override
+    public Object visit(ASTBoolValue node, Object data) {
+        ((DataStruct) data).type = VarType.Bool;
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTIdentifier node, Object data) {
+
+        if (node.jjtGetParent() instanceof ASTGenValue) {
+            String varName = node.getValue();
+            VarType varType = SymbolTable.get(varName);
+
+            ((DataStruct) data).type = varType;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTIntValue node, Object data) {
+        ((DataStruct) data).type = VarType.Number;
+        return null;
+    }
+
+
+    //des outils pour vous simplifier la vie et vous enligner dans le travail
+    public enum VarType {
+        Bool,
+        Number
+    }
+
+
+    private class DataStruct {
+        public VarType type;
+
+        public DataStruct() {
+        }
+
+        public DataStruct(VarType p_type) {
+            type = p_type;
+        }
+
+    }
+}
